1d8da308079b7f85e0dbb1a229de62c234d63056
Mario Conti <marioConti91@gmail.com>
2021-11-01 20:12:25 -0300

    se agrega funcion menor

--- src/Clase-2/math-utils.js
+++ src/Clase-2/math-utils.js
@@ -1,13 +1,31 @@
 import React from "react";
 
 export const ClaseDos = () => {
-  const Mayor = (a, b) => {
+  const mayor = (a, b) => {
     let mayor = a > b ? a : b;
+
+    // OTRA SOLUCION DE FAUSTO
+    const maxNum = (...nums) => {
+      let max = null;
+      nums.forEach((num) => {
+        if (max === null) {
+          return (max = num);
+        } else if (num > max) return (max = num);
+      });
+      return max;
+    };
+    console.log(maxNum(2, 5, 1, 29));
+// =========================================================
+
+const menor = (...nums) => Math.min(...nums)
+
+console.log(menor(2,3,4,8))
+
     return mayor;
   };
   return (
     <>
-      <p>El numero mayor es: {Mayor(2, 1)}</p>
+      <p>El numero mayor es: {mayor(2, 1)}</p>
     </>
   );
 };
