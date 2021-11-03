ef9cd2b682302d7fefce14e4e370ac0892802bc9
Mario Conti <marioConti91@gmail.com>
2021-11-03 20:53:57 -0300

    se agrega una a

--- src/Clase-2/math-utils.js
+++ src/Clase-2/math-utils.js
@@ -3,29 +3,28 @@ import React from "react";
 export const ClaseDos = () => {
   const mayor = (a, b) => {
     let mayor = a > b ? a : b;
-
-    // OTRA SOLUCION DE FAUSTO
-    // UNA
-    // const menor = (...nums) => Math.max(...nums)
-    // OTRA LARGA
-    // const maxNum = (...nums) => {
-    //   let max = null;
-    //   nums.forEach((num) => {
-    //     if (max === null) {
-    //       return (max = num);
-    //     } else if (num > max) return (max = num);
-    //   });
-    //   return max;
-    // };
-    // console.log(maxNum(2, 5, 1, 29));
-// =========================================================
-
-const menor = (...nums) => Math.min(...nums)
-
-console.log(menor(2,3,4,8))
-
+    console.log(menor(2, 3, 4, 8));
     return mayor;
   };
+
+  // OTRA SOLUCION DE FAUSTO
+  // UNA
+  // const menor = (...nums) => Math.max(...nums)
+  // OTRA LARGA
+  // const maxNum = (...nums) => {
+  //   let max = null;
+  //   nums.forEach((num) => {
+  //     if (max === null) {
+  //       return (max = num);
+  //     } else if (num > max) return (max = num);
+  //   });
+  //   return max;
+  // };
+  // console.log(maxNum(2, 5, 1, 29));
+  // =========================================================
+
+  const menor = (...nums) => Math.min(...nums);
+
   return (
     <>
       <p>El numero mayor es: {mayor(2, 1)}</p>
