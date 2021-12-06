4ce366a5c3578dc7b965e2b5784c6e439edc24e7
Mario Conti <marioconti91@gmail.com>
2021-12-06 17:49:11 -0300

    testing

--- src/esImpar/esImpar.test.js
+++ src/esImpar/esImpar.test.js
@@ -2,5 +2,5 @@ import { esImpar } from "./esImpar";
 
 test("Si ingresó un número impar el resultado debería ser true", () => {
   const result = esImpar(3);
-  expect(result).tobe(true);
+  expect(result).toBe(true);
 });
